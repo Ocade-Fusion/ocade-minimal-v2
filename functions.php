23f0784986ce0595294364303c8075f8a0789c4c
Valentin Charrier <valentin.charrier@hotmail.fr>
2025-02-09 19:01:28 +0100

    CP GIT

---
+++ functions.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace OcadeMinimal;
+
+$ORGANISATION_GITHUB = 'Ocade-Fusion';
+$DEPOT_GITHUB = 'ocade-minimal';
+$OCADE_THEME_REPO = 'https://github.com/' . $ORGANISATION_GITHUB . '/' . $DEPOT_GITHUB;
+$OCADE_VERSION_URL = $OCADE_THEME_REPO . '/releases/latest/download/version.txt';
+$OCADE_ZIP_URL = $OCADE_THEME_REPO . '/releases/latest/download/' . $DEPOT_GITHUB . '.zip';
+$OCADE_REMOTE_VERSION = $DEPOT_GITHUB . '_remote_version';
+$OCADE_ICON_SVG_URL = 'https://raw.githubusercontent.com/' . $ORGANISATION_GITHUB . '/' . $DEPOT_GITHUB . '/master/assets/icons/icon.svg';
+$OCADE_ICON_1X_URL =  'https://raw.githubusercontent.com/' . $ORGANISATION_GITHUB . '/' . $DEPOT_GITHUB . '/master/assets/icons/icon-1x.png';
+$OCADE_ICON_2X_URL =  'https://raw.githubusercontent.com/' . $ORGANISATION_GITHUB . '/' . $DEPOT_GITHUB . '/master/assets/icons/icon-2x.png';
+$OCADE_ICON_3X_URL =  'https://raw.githubusercontent.com/' . $ORGANISATION_GITHUB . '/' . $DEPOT_GITHUB . '/master/assets/icons/icon-3x.png';
+$OCADE_ICON_4X_URL =  'https://raw.githubusercontent.com/' . $ORGANISATION_GITHUB . '/' . $DEPOT_GITHUB . '/master/assets/icons/icon-4x.png';
+$OCADE_ICON_5X_URL =  'https://raw.githubusercontent.com/' . $ORGANISATION_GITHUB . '/' . $DEPOT_GITHUB . '/master/assets/icons/icon-5x.png';
+
+add_filter('site_transient_update_themes', function ($transient) {
+  if (!is_object($transient)) $transient = new stdClass();
+
+  $theme = wp_get_theme();
+  $theme_slug = $theme->get_stylesheet();
+  $current_version = $theme->get('Version');
+  $remote_version = get_transient($OCADE_REMOTE_VERSION);
+  $response = wp_remote_get($OCADE_VERSION_URL);
+  if (is_wp_error($response)) return $transient;
+  $remote_version = trim(wp_remote_retrieve_body($response));
+  $remote_version = preg_replace('/[^0-9.]/', '', $remote_version);
+  set_transient($OCADE_REMOTE_VERSION, $remote_version, 6 * HOUR_IN_SECONDS);
+
+  // Comparaison des versions
+  if (version_compare($remote_version, $current_version, '>')) {
+    if (!isset($transient->response)) $transient->response = []; // Assurez-vous que c'est un tableau
+    $transient->response[$theme_slug] = [
+      'theme'       => $theme_slug,
+      'new_version' => $remote_version,
+      'url'         => $OCADE_THEME_REPO,
+      'package'     => $OCADE_ZIP_URL,
+      'icons'       => [
+        'svg'  => $OCADE_ICON_SVG_URL, 
+        '1x'   => $OCADE_ICON_1X_URL,  
+        '2x'   => $OCADE_ICON_2X_URL,  
+        '3x'   => $OCADE_ICON_3X_URL,  
+        '4x'   => $OCADE_ICON_4X_URL,  
+        '5x'   => $OCADE_ICON_5X_URL,  
+      ],
+    ];
+  }
+  return $transient;
+});
+
+add_action('upgrader_process_complete', function ($upgrader_object, $options) {
+  if ($options['action'] === 'update' && $options['type'] === 'theme') delete_transient(OCADE_REMOTE_VERSION);
+}, 10, 2);
